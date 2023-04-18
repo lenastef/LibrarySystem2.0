db08ac226162bc73e62d2290aec942262201a4a9
helena stefansdottir <hel>
2023-04-18 10:59:20 +0000

    Test files

--- src/test/java/LibrarySystem.java
+++ src/test/java/LibrarySystem.java
@@ -1,5 +1,3 @@
 public class LibrarySystem {
-    public static void main(String[] args) {
-
-    }
+   
 }
--- src/test/java/TestStudent.java
+++ src/test/java/TestStudent.java
@@ -1,5 +1,3 @@
 public class TestStudent {
-    public static void main(String[] args) {
 
-    }
 }
